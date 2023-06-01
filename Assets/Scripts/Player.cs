3fae10083ba6286eaf60984edaca23b81149f05b
SzymonBrodziński <62517155+SzymBrodz@users.noreply.github.com>
2023-06-01 16:35:30 +0200

    F2/tileset (#1)
    * Added dungeon tileset
    
    -added dungeon tileset
    -need to scale A* pathfinding
    
    * basic setup
    
    * new tileset
    
    -new tile set added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,70 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    Vector3 moveDelta;
     public int keys_collected = 0;
+    //public Rigidbody2D rb;
 
+    private RaycastHit2D hit;
+
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
 
     void Update()
     {
-        float moveAmountVer = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        float moveAmountHor = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        transform.Translate(0,moveAmountVer,0);
-        transform.Translate(moveAmountHor, 0, 0);
+        //float moveAmountVer = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        //float moveAmountHor = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //transform.Translate(0,moveAmountVer,0);
+        //transform.Translate(moveAmountHor, 0, 0);
 
 
     }
 
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDelta = new Vector3(x, y, 0);
+
+        Debug.Log(x);
+        Debug.Log(y);
+
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        //check if can move in the vertical director by casting a collider box there. If null -> can move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Walls"));
+
+        if (hit.collider == null)
+        {
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Walls"));
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
+    }
+
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
+
+
         if(other.gameObject.tag == "Walls")
         {
             //TODO utrata hp
