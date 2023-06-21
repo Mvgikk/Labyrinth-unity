f54e4ebca22e660331d5bbd4d9fda39fe6e00547
Mvgikk <67462828+Mvgikk@users.noreply.github.com>
2023-06-21 14:05:54 +0200

    monster run animation

---
+++ Assets/Scripts/DemonController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+
+    private Vector3 previousPosition;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x != previousPosition.x)
+        {
+            
+            animator.SetFloat("HorizontalSpeed",1);
+
+
+            previousPosition = transform.position;
+        }
+
+        if (transform.position.y != previousPosition.y)
+        {
+            
+            animator.SetFloat("VerticalSpeed",1);
+
+
+            previousPosition = transform.position;
+        }
+        
+    }
+}
